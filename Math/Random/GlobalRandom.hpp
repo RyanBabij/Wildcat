dd8dee2a2f3c8d00ff00c37c7ab7e51f8c9af3d2
RyanBabij <rbabij@gmail.com>
2019-02-27 21:58:30 +1030

    Cleanup GlobalRandom

--- Math/Random/GlobalRandom.hpp
+++ Math/Random/GlobalRandom.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef GLOBALRANDOM_HPP
+#define GLOBALRANDOM_HPP
 
 /*
 #include <Math/Random/GlobalRandom.hpp>
@@ -9,14 +11,11 @@ RNG is global to save performance for game development. Also good for preserving
 0272407085 - Replaced external Mersenne Twister with built-in version. External one was causing random crashes with certain compiler optimisations. Took about 4 hours to track it down.
 
 C++11 introduced a lot of built-in functionality regarding random number generation (remember std::rand lol). Unfortunately it means that a lot of this code needs to be reworked. There also seems to be some kind of elusive bug caused by using the static Random class, not sure if it's thread-related or what. For now I'm sticking with the non-static class, but yeah it's a mess.
+
+Oh man, I just noticed the same RNG is being used for both static and non-static instances. That was fun. Better to have a defined static function for global shared rng.
 */
 
-#ifndef GLOBALRANDOM_HPP
-#define GLOBALRANDOM_HPP
 
-//#include <Math/Random/MersenneTwister.hpp>
-
-// We must migrate to c++ standard mersenne twister.
 
 // Mersenne twister is now part of c++
 #include <random>
@@ -24,51 +23,36 @@ C++11 introduced a lot of built-in functionality regarding random number generat
 
 #include <time.h> /* Required to call time() */
 
-//std::mt19937 gen (123);
-		//std::uniform_real_distribution<double> rnd(0,255);
-		//int rand = dis(generator);
-		
-//std::mt19937::result_type seed2 = time(0);
-//auto rand2 = std::bind(std::uniform_int_distribution<int>(0,255),
-                          // std::mt19937(seed2));
-
+  // NOTE THAT THIS IS STATIC. USE RandomNonStatic when you are using predictable seeding.
+  // This RNG is global. Basically it just makes life more convenient when you can seed once
+  // and then call Random:: whenever you want a random number.
 class Random
 {
-	public:
-	//static MTRand r;
+	private:
 	static std::mt19937 r2;
 
+  public:
+  
 	inline static int randomInt(const int maxVal)
 	{
-		//return r.randInt(maxVal);
-	
-
-		// std::mt19937::result_type seed2 = time(0);
-		// std::mt19937 rng (seed2);
 		std::uniform_int_distribution<int> randomDistribution(0,maxVal);
 		return randomDistribution(r2);
 	}
 	inline static int randInt(const int maxVal)
 	{ return randomInt(maxVal); }
 
+    // Seed using an int
 	void seed (const int s)
-	{
-		//r.seed(s);
-		r2.seed(s);
-	}
-
-		//0231263121. THIS SHOULD SEED RANDOMLY...
+	{ r2.seed(s); }
+  
+    // By default we seed using time
 	static void seed ()
-	{
-		//r.seed();
-		r2.seed(time(0));
-	}
+	{ r2.seed(time(0)); }
 
 	inline static double randDouble (const double maxVal)
 	{
 		std::uniform_real_distribution<double> randomDistribution(0,maxVal);
 		return randomDistribution(r2);
-		//return r.rand(maxVal);
 	}
 	inline static double randomDouble (const double maxVal)
 	{ return randDouble(maxVal); }
@@ -79,9 +63,6 @@ class Random
 	{
 		if (_min==_max) { return _min; }
 
-
-		//int range = max-min;
-		//int _ret = (r.randInt(range)) + min;
 		return (randInt(_max-_min)) + _min;
 	}
 		static int range ( const int _min, const int _max)
@@ -90,12 +71,7 @@ class Random
 
 	/* Will return true one in 'prob' times. Ie, with prob at 100, will return true roughly 1 in 100 times. */
 	static bool oneIn(const int prob)
-	{
-		return (randInt(prob)==0);
-		//if(r.randInt(prob)==0) { return true; }
-		//else { return false; }
-		//return false;
-	}
+	{ return (randInt(prob)==0); }
 
 	/* Shorthand for randInt(). */
 	static int roll(const int _maxVal)
@@ -134,7 +110,7 @@ class Random
 		}
 		return false;
 	}
-	
+    // Lol what the hell is this
 	static double randNorm (double a, double b)
 	{
 		return 7;
@@ -149,40 +125,30 @@ std::mt19937 Random::r2;
 class RandomNonStatic
 {
 	private:
-	std::mt19937 r2;
+	std::mt19937 rng;
 	
 	public:
 
 	inline int randomInt(const int maxVal)
 	{
-		//return r.randInt(maxVal);
-	
-
-		// std::mt19937::result_type seed2 = time(0);
-		// std::mt19937 rng (seed2);
 		std::uniform_int_distribution<int> randomDistribution(0,maxVal);
-		return randomDistribution(r2);
+		return randomDistribution(rng);
 	}
 	inline int randInt(const int maxVal)
 	{ return randomInt(maxVal); }
 
+    // Seed using an int
 	void seed (const int s)
-	{
-		r2.seed(s);
-	}
-
-		//0231263121. THIS SHOULD SEED RANDOMLY...
-	void seed ()
-	{
-		//r.seed();
-		r2.seed(time(0));
-	}
+	{ rng.seed(s); }
+  
+    // By default we seed using time
+  void seed ()
+	{ rng.seed(time(0)); }
 
 	inline double randDouble (const double maxVal)
 	{
 		std::uniform_real_distribution<double> randomDistribution(0,maxVal);
-		return randomDistribution(r2);
-		//return r.rand(maxVal);
+		return randomDistribution(rng);
 	}
 	inline double randomDouble (const double maxVal)
 	{ return randDouble(maxVal); }
@@ -192,10 +158,6 @@ class RandomNonStatic
 	int randomIntRange(const int _min, const int _max)
 	{
 		if (_min==_max) { return _min; }
-
-
-		//int range = max-min;
-		//int _ret = (r.randInt(range)) + min;
 		return (randInt(_max-_min)) + _min;
 	}
 		int range ( const int _min, const int _max)
@@ -204,12 +166,7 @@ class RandomNonStatic
 
 	/* Will return true one in 'prob' times. Ie, with prob at 100, will return true roughly 1 in 100 times. */
 	bool oneIn(const int prob)
-	{
-		return (randInt(prob)==0);
-		//if(r.randInt(prob)==0) { return true; }
-		//else { return false; }
-		//return false;
-	}
+	{ return (randInt(prob)==0); }
 
 	/* Shorthand for randInt(). */
 	int roll(const int _maxVal)
