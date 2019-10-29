54419b2605a3be809d84996ed8067a6769a50e7d
RyanBabij <rbabij@gmail.com>
2019-10-29 10:58:19 +1030

    Update Font
    * Add support for char arrays

--- Graphics/Font/Font.hpp
+++ Graphics/Font/Font.hpp
@@ -289,6 +289,169 @@ class Font
     return linesDrawn;
   }
 	
+	//Pass a character array. Array can be 2D, but rows won't be respected as newlines unless the coordinates are set manually to align.
+	int drawText(char* text, unsigned int arraySize, unsigned int x1, unsigned int y1, unsigned int x2, unsigned int y2, bool centeredX = false, bool centeredY=false, bool rawTextMode=false,
+				unsigned char _colourRed=0, unsigned char _colourGreen=0,unsigned char _colourBlue=0)
+	{
+		if (loadSuccess == false )
+		{
+			return 0;
+		}
+		
+		/* Automatically sort the box coordinates here, so we don't need to input the coordinates in any particular order. */
+		if (y1<y2) { DataTools::swap(&y1,&y2); }
+		if (x1>x2) { DataTools::swap(&x1,&x2); }
+
+		const int panelWidth = x2-x1;
+		const int panelHeight = y1-y2;
+
+		if ( panelWidth < nX || panelHeight < nY || arraySize == 0 )
+		{
+			return 0;
+		}
+    
+    int linesDrawn = 1;
+
+
+		Renderer::setTextureMode();
+    //Renderer::setColour(_colourRed,_colourGreen,_colourBlue);
+    glColor3ub(_colourRed,_colourGreen,_colourBlue);
+
+		const int charsPerLine = (x2-x1) / nX;
+		
+		unsigned int currentX=x1;
+		unsigned int currentY=y1;
+		
+		/* Center the initial line, by altering the starting coordinate for the first character. */
+		if(centeredX==true)
+		{
+			//unsigned int lineWidth = 0;
+			unsigned int currentX2 = x1;
+			/* Figure out how many characters fit on this line. */
+			for( unsigned int i=0;i<arraySize  && currentX2+nX <= x2;++i)
+			{
+				//lineWidth+=nX;
+				currentX2+=nX;
+			}
+			
+			int remainderPixels = x2 - currentX2;
+			
+			
+			currentX = x1+(remainderPixels/2);
+		}
+		
+		if(centeredY==true)
+		{
+			int nLines = (arraySize/charsPerLine)+1;
+			int vSpace = nLines*(nY+ySpacing)-ySpacing;
+			
+			int sY = y1-y2;
+			currentY = y1 - ((sY-vSpace)/2);
+
+		}
+		
+		for(unsigned int i=0;i<arraySize;++i)
+		{
+
+
+
+				// No spaces when going to newline.
+			if ( currentX==x1 )
+			{
+				if (text[i]==' ' && rawTextMode == false)
+				{
+					++i;
+				}
+			}
+		
+				
+			// SET THE COLOUR
+      // Protip: Changing colour doesn't work on black.
+        glColor3ub(_colourRed,_colourGreen,_colourBlue);
+				//glColor3ub(250,0,0);
+				//glEnable(GL_COLOR_MATERIAL);
+
+				if ( text[i]!='\n' )
+				{
+					glBindTexture(GL_TEXTURE_2D, character[(int)text[i]]);
+					glBegin(GL_QUADS);
+					glTexCoord2f(0,1);
+					glVertex2f(currentX,currentY-nY);
+					glTexCoord2f(1,1);
+					glVertex2f(currentX+nX,currentY-nY);
+					glTexCoord2f(1,0);
+					glVertex2f(currentX+nX,currentY);
+					glTexCoord2f(0,0);
+					glVertex2f(currentX,currentY);
+					glEnd();
+				}
+		
+		
+
+				
+				currentX+=nX;
+
+				/* If we have reached x2, then start a new line. */
+				if (currentX+nX > x2 || text[i]=='\n')
+				{
+					currentX=x1;
+					currentY-=(nY+ySpacing);
+					++linesDrawn;
+					
+					
+					/* Center the new line. */
+					if(centeredX==true)
+					{
+						unsigned int currentX2 = x1;
+						/* Figure out how many characters fit on this line. */
+						for( unsigned int i2=i;i2<arraySize && currentX2+nX <= x2;++i2)
+						{
+							currentX2+=nX;
+						}
+						int remainderPixels = x2 - currentX2;
+						
+						currentX = x1+(remainderPixels/2);
+						
+					
+					}
+
+				}
+
+
+				// I think this breaks early if writing  a long word which will break
+
+				if ( rawTextMode == false && arraySize > 16 && text[i]==' ' )
+				{
+					int charsUntilSpace = 0;
+					for (unsigned int i2=1; i2<20&&i2+i<arraySize;++i2)
+					{
+						if ( text[i2+i] == ' ' )
+						{
+							break;
+						}
+						++charsUntilSpace;
+					}
+					const int spaceRequired = nX*charsUntilSpace+nX;
+					if ( spaceRequired < panelWidth && charsUntilSpace > 0 )
+					{
+						if ( currentX + spaceRequired > x2 )
+						{
+							currentX=x1;
+							currentY-=(nY+2);
+              ++linesDrawn;
+						}
+					}
+				}
+
+				/* If we are out of space, return. */
+				if (currentY-nY < y2)
+				{     glColor3ub(255,255,255); return linesDrawn; }
+		}
+		//glColor3ub(255,255,255);
+		    glColor3ub(255,255,255);
+    return linesDrawn;
+	}
+	
 	void drawText(const std::string text, int x1, int y1)
 	{
 		if (loadSuccess == false )
@@ -317,7 +480,7 @@ class Font
     glColor3ub(255,255,255);
 	}
 	
-	
+
 	
 };
 }
