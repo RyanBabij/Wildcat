ac7d3d996c08045197df5bfa68ddd644b01bec08
Ryan Babij <rbabij@gmail.com>
2019-12-27 13:54:46 +0800

    Update Shunting.cpp
    * Add parentheses processing to new Token system
    * Cleanup and documentation
    * Add test cases

--- Algorithm/Shunting.cpp
+++ Algorithm/Shunting.cpp
@@ -3,20 +3,49 @@
 #define WILDCAT_ALGORITHM_SHUNTING_CPP
 
 /* Wildcat: Algorithm/Shunting.cpp
-   #include "Algorithm/Shunting.cpp"
+   #include <Algorithm/Shunting.cpp>
+   
+   By Ryan Babij (github.com/RyanBabij)
    
    Implementation of Edsger Dijkstra's shunting-yard algorithm.
+   This algorithm converts infix mathematical notation into
+   postfix notation, which can be processed in linear order, and
+   therefore is much easier to process algorithmically. However
+   postfix notation requires a delimiter to identify multiple
+   adjacent values.
    
+   May have some bugs because I am not good at math.
+   I'm not sure how negative numbers are handled in this system,
+   but basic testing seems to be giving usable results.
+   
+   Sample input:
+   
+   3+4*2/(1-5)^2^3      -> 2 3 * 3 4 * + 
+   (2*3+3*4)            -> 2 3 * 3 4 * +
+   20-30/3+4*2^3        -> 20 30 3 / - 4 2 3 ^ * +
+   (-1+1+(-1*2))*(-2*1) -> 1 - 1 + 1 2 * - + 2 1 * - *
+   
+
+   Credit to Takayuki MATSUOKA's implementation at
+   https://gist.github.com/t-mat/b9f681b7591cdae712f6 and
+   https://ideone.com/DYX5CW
+   
+   Todo: Test custom operator support.
+
 */
 
+//#define SHUNTING_ENABLE_OUTPUT //cout result
+
+// can store either a value or an operator. Operators must be a single
+// non-digit char. virtual function operate() defines functionality
 class Shunting_Token
 {
    public:
    
    unsigned char symbol; // if set to 0, then token is a value.
    bool rightAssociative;
-   unsigned short int precedence;
-   long int value;
+   unsigned short int precedence; // higher = higher precedence
+   long int value; // only used for non-operator token
    
    Shunting_Token(int _value, unsigned char _symbol = 0, unsigned short int _precedence = 0, bool _rightAssociative = false)
    {
@@ -44,6 +73,8 @@ class Shunting_Token
       return DataTools::toString(value);
    }
 };
+
+//     DEFAULT OPERATOR SET
 
 class Shunting_Token_Add: public Shunting_Token
 {
@@ -122,91 +153,29 @@ class Shunting_Token_RightParen: public Shunting_Token
    { return 0; }
 };
 
-// Custom operators can be defined using this interface.
-// Special cases are right and left parenthesis.
-class Shunting_Operator
-{
-   public:
-   char symbol;
-   bool rightAssociative;
-   int precedence;
-   
-   Shunting_Operator(char _symbol, int _precedence, bool _rightAssociative = false)
-   {
-      symbol=_symbol;
-      rightAssociative = _rightAssociative;
-      precedence = _precedence;
-      
-   }
-   
-   // define custom operation here.
-   virtual int operation (int lhs, int rhs)
-   {
-      return 0;
-   }
-   
-   
-   // for vector contains() call
-   // void operator == (const Shunting_Operator &rhs )
-   // { 
-      // return symbol == rhs.symbol;
-   // }
-      
-};
-
+// Main driver class. Initialise and create operators, or call
+// buildDefaults() to use default set. Call shunt() and pass
+// infix string. It will return vector of tokens in postfix.
+// No whitespace, only pass digits and operators.
 class Shunting
 {
    public:
+
+   Vector <Shunting_Token> vTokenList; // reference list of all tokens
    
-   // define operators here
-   Vector <Shunting_Operator> vOperator;
-   
-   Vector <Shunting_Token> vToken;
-   Vector <Shunting_Token> vTokenList;
-   
-   
-   std::deque<std::string> outputQueue;
-   std::deque<Shunting_Token> outputQueue2;
-   
-   std::vector<Shunting_Operator> stack;
-   std::vector<Shunting_Token> stack2;
+   std::deque<Shunting_Token> outputQueue2; // output queue
+
+   std::vector<Shunting_Token> stack2; // operator stack
    
    
    Shunting()
-   {
-   }
+   { }
    ~Shunting()
-   {
-   }
+   { }
    
-   // Only pure expressions should be passed here. No variables, strings or whitespace.
-   // Functions will need to be custom, probably replaced by a char.
-   // Expressions to test:
-   // (1+2+3)
-   // (1+2)/2*2
-   // 3+4*2/(1-5)^2^3
-   // 20-30/3+4*2^3
-   
-   void addOperator(Shunting_Operator _op)
-   {
-      vOperator.push(_op);
-   }
    void addOperator2(Shunting_Token _toke)
    {
       vTokenList.push(_toke);
-   }
-   
-   Shunting_Operator* getOperator(unsigned char _symbol)
-   {
-      //Shunting_Operator* retOperator = 0;
-      for (int i=0;i<vOperator.size();++i)
-      {
-         if ( vOperator(i).symbol == _symbol )
-         {
-            return &vOperator(i);
-         }
-      }
-      return 0;
    }
    
    Shunting_Token * getToken(unsigned char _symbol)
@@ -231,23 +200,16 @@ class Shunting
             return true;
          }
       }
-      
-      // for (int i=0;i<vOperator.size();++i)
-      // {
-         // if (vOperator(i).symbol == _op)
-         // {
-            // return true;
-         // }
-      // }
       return false;
    }
    
    
    std::deque <Shunting_Token> shunt(std::string expression)
    {
-      std::cout<<"Pre-shunt: "<<expression<<".\n";
-      
-      outputQueue.clear();
+      #ifdef SHUNTING_ENABLE_OUTPUT
+         std::cout<<"Input: "<<expression<<"\n";
+      #endif
+
       outputQueue2.clear();
       
       std::string _strNumber = "";
@@ -257,166 +219,99 @@ class Shunting
       {
          if ( std::isdigit(expression[i]) )
          {
-            std::cout<<"digit\n";
             _strNumber+=expression[i];
          }
          else if ( isOperator(expression[i]))
          {
-            std::cout<<"operator\n";
             if ( _strNumber.size() > 0 )
             {
-               // push number to the output queue
-               outputQueue.push_back(_strNumber);
-               
-               //2: push a value token to the output queue
-               //Shunting_Token st (DataTools::toInt(_strNumber));
+               // push a value token to the output queue
                outputQueue2.push_back(Shunting_Token(DataTools::toInt(_strNumber)));
                
                _strNumber="";
             }
             
-            Shunting_Token * st = getToken(expression[i]);
-            
-            if ( st == 0 )
+            Shunting_Token * currentToken = getToken(expression[i]);
+            if ( currentToken == 0 )
             {
-               std::cout<<"ERROR: Invalid symbol.\n";
-               
-                  //return "";
+               //something bad happened, return an empty output vector.
+               outputQueue2.clear();
+               return outputQueue2;
             }
-            std::cout<<"Current token symbol: "<<st->symbol<<".\n";
-            
-            
-            //2: process basic operators
-            // push operator
-            // If the token is operator, o1, then:
-            //const auto o1 = token;
 
-            // while there is an operator token,
-            while(!stack2.empty())
-            {
-               // o2, at the top of stack, and
-               const Shunting_Token o2 = stack2.back();
-
-               // // either o1 is left-associative and its precedence is
-               // // *less than or equal* to that of o2,
-               // // or o1 if right associative, and has precedence
-               // // *less than* that of o2,
-               if ( (! st->rightAssociative && st->precedence <= o2.precedence)
-               ||   (  st->rightAssociative && st->precedence <  o2.precedence) )
-               {
-                  // // // then pop o2 off the stack,
-                  stack2.pop_back();
-                  // // // onto the output queue;
-                  // //std::string strQ (1,o2.symbol);
-                  // //std::string strQ = 'a';
-                  //outputQueue2.push_back(std::string(1,o2.symbol));
-                  outputQueue2.push_back(o2);
-
-                  continue;
-               }
-               //otherwise exit
-               break;
-            }
-            //push current operator onto stack
-            stack2.push_back(*st);
-            
-            
-            
-            
-
-            Shunting_Operator * currentOperator = getOperator(expression[i]);
-            if ( currentOperator == 0 )
-            {
-               //something bad happened
-               std::cout<<"ERROR\n";
-                  //return "";
-            }
-            std::cout<<"Current operator symbol: "<<currentOperator->symbol<<".\n";
-            
             //push left paren onto stack
             if ( expression[i] == '(' )
             {
-               std::cout<<"Pushing left paren\n";
-               //Shunting_Operator sh ('(',5,false);
-               stack.push_back(*currentOperator);
+               stack2.push_back(*currentToken);
             }
             else if (expression[i] == ')')
             {
-               std::cout<<"Right paren\n";
-               
                 bool match = false;
 
                 // Until the token at the top of the stack
                 // is a left parenthesis,
-                while(! stack.empty() && stack.back().symbol != '(')
+                while(! stack2.empty() && stack2.back().symbol != '(')
                 {
                     // pop operators off the stack
                     // onto the output queue.
-                    outputQueue.push_back(std::string(1,stack.back().symbol));
-                    stack.pop_back();
+                    outputQueue2.push_back(stack2.back());
+                    stack2.pop_back();
                     
                 }
-                if (!stack.empty() && stack.back().symbol == '(')
+                if (!stack2.empty() && stack2.back().symbol == '(')
                 {
                   match = true;
                 }
 
                 // Pop the left parenthesis from the stack,
                 // but not onto the output queue.
-                std::cout<<"Popping: "<<(unsigned char)stack.back().symbol<<".\n";
-                stack.pop_back();
+                stack2.pop_back();
 
-                if(!match && stack.empty())
+                if(!match && stack2.empty())
                 {
-                    // If the stack runs out without finding a left parenthesis,
-                    // then there are mismatched parentheses.
-                    std::cout<<"ERROR: Too many right paren.\n";
-                     //return "";
+                  // too many right parentheses. Return empty output vector.
+                    outputQueue2.clear();
+                    return outputQueue2;
                 }
-               
             }
             else
             {
-               
-               
                // push operator
                // If the token is operator, o1, then:
-               //const auto o1 = token;
 
                // while there is an operator token,
-               while(!stack.empty())
+               while(!stack2.empty())
                {
                   // o2, at the top of stack, and
-                  const Shunting_Operator o2 = stack.back();
+                  const Shunting_Token o2 = stack2.back();
 
                   // either o1 is left-associative and its precedence is
                   // *less than or equal* to that of o2,
                   // or o1 if right associative, and has precedence
                   // *less than* that of o2,
-                  if ( (! currentOperator->rightAssociative && currentOperator->precedence <= o2.precedence)
-                  ||   (  currentOperator->rightAssociative && currentOperator->precedence <  o2.precedence) )
+                  if ( (! currentToken->rightAssociative && currentToken->precedence <= o2.precedence)
+                  ||   (  currentToken->rightAssociative && currentToken->precedence <  o2.precedence) )
                   {
                      // // then pop o2 off the stack,
-                     stack.pop_back();
+                     stack2.pop_back();
                      // // onto the output queue;
-                     //std::string strQ (1,o2.symbol);
-                     //std::string strQ = 'a';
-                     outputQueue.push_back(std::string(1,o2.symbol));
-
+                     outputQueue2.push_back(o2);
                      continue;
                   }
                   //otherwise exit
                   break;
                }
                //push current operator onto stack
-               stack.push_back(*currentOperator);
+               stack2.push_back(*currentToken);
                
                
             }
          }
          else
          {
-            // invalid expression
+            // invalid expression, return empty output vector
+            outputQueue2.clear();
+            return outputQueue2;
          }
          
       }
@@ -424,63 +319,45 @@ class Shunting
       //final case: Push any remaining number
       if (_strNumber.size() > 0)
       {
-         outputQueue.push_back(_strNumber);
          outputQueue2.push_back(Shunting_Token(DataTools::toInt(_strNumber)));
          _strNumber="";
       }
       
-         //final case: No more tokens to read, so push the remaining stack
-    // When there are no more tokens to read:
-    //   While there are still operator tokens in the stack:
-      while(! stack.empty())
+      //final case: No more tokens to read, so push the remaining stack
+      // When there are no more tokens to read:
+      //   While there are still operator tokens in the stack:
+      while(! stack2.empty())
       {
          // If the operator token on the top of the stack is a parenthesis,
          // then there are mismatched parentheses.
-         if(stack.back().symbol == '(')
+         if(stack2.back().symbol == '(')
          {
-            std::cout<<"ERROR: Parenthesis mismatch.\n";
-               //return "";
+            // too many left parentheses, return empty output vector.
+            outputQueue2.clear();
+            return outputQueue2;
          }
 
          // Pop the operator onto the output queue.
-         //outputQueue.push_back(stack.back().symbol);
-         outputQueue.push_back(std::string(1,stack.back().symbol));
          outputQueue2.push_back(stack2.back());
-         
          stack2.pop_back();
-         stack.pop_back();
       }
 
       
-      
-      std::cout<<"Post-shunt: ";
-      
-      for(unsigned int i=0;i<outputQueue.size();++i)
-      {
-         std::cout<<outputQueue.at(i);
-      } std::cout<<"\n";
-      
-      std::cout<<"New shunt: ";
-      
-      for (unsigned int i=0;i<outputQueue2.size();++i)
-      {
-         std::cout<<outputQueue2.at(i).toString();
-      } std::cout<<"\n";
+      #ifdef SHUNTING_ENABLE_OUTPUT
+         std::cout<<"Output: ";
+         for (unsigned int i=0;i<outputQueue2.size();++i)
+         {
+            std::cout<<outputQueue2.at(i).toString()<<" ";
+         } std::cout<<"\n";
+      #endif
       
       return outputQueue2;
    }
    
+   // add the standard set of non-function operators:
+   // +-*/^()
    void buildDefaults()
    {
-      addOperator(Shunting_Operator('^',4,true));
-      addOperator(Shunting_Operator('*',3,false));
-      addOperator(Shunting_Operator('/',3,false));
-      addOperator(Shunting_Operator('+',2,false));
-      addOperator(Shunting_Operator('-',2,false));
-      addOperator(Shunting_Operator('(',0,false));
-      addOperator(Shunting_Operator(')',0,false));
-      
-      
       addOperator2(Shunting_Token_Add());
       addOperator2(Shunting_Token_Subtract());
       addOperator2(Shunting_Token_Multiply());
@@ -488,6 +365,28 @@ class Shunting
       addOperator2(Shunting_Token_Power());
       addOperator2(Shunting_Token_LeftParen());
       addOperator2(Shunting_Token_RightParen());
+   }
+   
+   // runs a bunch of test-cases to demonstrate it works.
+   // see documentation for expected output.
+   void test()
+   {
+      Vector <std::string> vTestCase = { "3+4*2/(1-5)^2^3", "(2*3+3*4)", "20-30/3+4*2^3", "(-1+1+(-1*2))*(-2*1)" };
+
+      for (int i=0;i<vTestCase.size();++i)
+      {
+         std::deque <Shunting_Token> vOutput = shunt(vTestCase(i));
+         
+         std::cout<<"Test input: "<<vTestCase(i)<<"\n";
+         
+         std::cout<<"Output: ";
+         for (unsigned int i2=0;i2<vOutput.size();++i2)
+         {
+            std::cout<<vOutput.at(i2).toString()<<" ";
+            
+         } std::cout<<"\n";
+         
+      }
    }
   
 };
