cfb4d18b5ce5a11cf46f2ed8a92fc90300286997
RyanBabij <rbabij@gmail.com>
2020-02-05 20:11:44 +1030

    Update AudioPlayer_OpenAL.hpp
    * Add some revolver sound functionality
    * Add some preloading functionality

--- Audio/AudioPlayer_OpenAL.hpp
+++ Audio/AudioPlayer_OpenAL.hpp
@@ -17,6 +17,98 @@
 #include <Audio/AudioPlayer.hpp>
 #include <Container/Vector/Vector.hpp>
 
+
+// load a bunch of sources to allow overlapping sounds
+class ALRevolver
+{
+   public:
+#ifdef WILDCAT_AUDIO
+   Vector <ALuint> vLoadedSource;
+   
+   int currentIndex;
+#endif
+   
+   ALRevolver()
+   {
+#ifdef WILDCAT_AUDIO
+      currentIndex=0;
+#endif
+   }
+   
+   void load(Sound* sound, int nDupes)
+   {
+#ifdef WILDCAT_AUDIO
+      
+      for (int i=0;i<nDupes;++i)
+      {
+
+         ALuint soundBuffer;
+         alGenBuffers(1, &soundBuffer);
+         
+         ALuint source;
+         alGenSources(1,&source);
+		
+         if(sound->bitsPerSample==8)
+         {
+            if(sound->nChannels==1)
+            { alBufferData(soundBuffer,AL_FORMAT_MONO8,sound->data,sound->nData,sound->samplesPerSecond); }
+            else if (sound->nChannels==2)
+            { alBufferData(soundBuffer,AL_FORMAT_STEREO8,sound->data,sound->nData,sound->samplesPerSecond); }
+            else
+            { std::cout<<"Error: WAV needs to have 1 or 2 channels.\n"; }
+         }
+         else if(sound->bitsPerSample==16)
+         {
+            if(sound->nChannels==1)
+            { alBufferData(soundBuffer,AL_FORMAT_MONO16,sound->data,sound->nData,sound->samplesPerSecond); }
+            else if (sound->nChannels==2)
+            { alBufferData(soundBuffer,AL_FORMAT_STEREO16,sound->data,sound->nData,sound->samplesPerSecond); }
+            else
+            { std::cout<<"Error: WAV needs to have 1 or 2 channels.\n"; }
+         }
+         else
+         { std::cout<<"Error: Sound needs to be 8 or 16 bit.\n"; }
+
+         
+
+         /* Load buffer into source. */
+         alSourcei(source, AL_BUFFER, soundBuffer);
+         /* Set volume. */
+         //const float volume = (float)globalVolume/100;
+         //alSourcef(source, AL_GAIN, volume);
+         alSourcef(source, AL_GAIN, 10);
+
+         vLoadedSource.push(source);
+         //return vLoadedSource.size()-1;
+         // vSound.push(sound);
+         // vSource.push(source);
+         
+         
+         
+      }
+		
+#endif
+   }
+   
+   void playRotate()
+   {
+#ifdef WILDCAT_AUDIO
+ //alSourceStop(vLoadedSource(currentIndex));
+      ++currentIndex;
+      if (currentIndex >= vLoadedSource.size() )
+      {
+         currentIndex=0;
+      }
+		/* Play source in new thread. */
+       std::cout<<"Playing: "<<currentIndex<<" with id "<<vLoadedSource(currentIndex)<<"\n";
+		alSourcePlay(vLoadedSource(currentIndex));
+      
+     
+#endif
+   }
+   
+};
+
 class AudioPlayer_OpenAL: public AudioPlayer
 {
 	public:
@@ -28,8 +120,16 @@ class AudioPlayer_OpenAL: public AudioPlayer
 	
 	Vector <Sound*> vSound;
 	Vector <ALuint> vSource;
+	Vector <ALuint> vBuffer;
+	
 	
 	bool closed; /* Prevent closing multiple times which can cause crash */
+   
+   Vector <ALuint> vLoadedSource;
+   Vector <ALuint> vLoadedBuffer;
+   
+   Vector <ALRevolver*> vRevolver;
+   
 #endif
 	
 	AudioPlayer_OpenAL()
@@ -41,7 +141,87 @@ class AudioPlayer_OpenAL: public AudioPlayer
 		closed = false;
 #endif
 	}
+   
+   int loadRevolver(Sound * sound, int nSounds)
+   {
+#ifdef WILDCAT_AUDIO
+      ALRevolver * alv = new ALRevolver();
+      alv->load(sound,nSounds);
+      vRevolver.push(alv);
+      return vRevolver.size()-1;
+#else
+   return 0;
+#endif
+   }
+   
+   void playRevolver(int id)
+   {
+#ifdef WILDCAT_AUDIO
+      vRevolver(id)->playRotate();
+#endif
+   }
+   
+   int load(Sound* sound)
+   {
+#ifdef WILDCAT_AUDIO
+		ALuint soundBuffer;
+		alGenBuffers(1, &soundBuffer);
+		
+		//std::cout<<"sound->bitsPerSample: "<<sound->bitsPerSample<<".\n";
+		
+		if(sound->bitsPerSample==8)
+		{
+			//std::cout<<"Nchannels: "<<sound->nChannels<<".\n";
+			//std::cout<<"8 bit.\n";
+			if(sound->nChannels==1)
+			{ alBufferData(soundBuffer,AL_FORMAT_MONO8,sound->data,sound->nData,sound->samplesPerSecond); }
+			else if (sound->nChannels==2)
+			{ alBufferData(soundBuffer,AL_FORMAT_STEREO8,sound->data,sound->nData,sound->samplesPerSecond); }
+			else
+			{ std::cout<<"Error: WAV needs to have 1 or 2 channels.\n"; }
+		}
+		else if(sound->bitsPerSample==16)
+		{
+			//std::cout<<"Nchannels: "<<sound->nChannels<<".\n";
+			//std::cout<<"16 bit.\n";
+			if(sound->nChannels==1)
+			{ alBufferData(soundBuffer,AL_FORMAT_MONO16,sound->data,sound->nData,sound->samplesPerSecond); }
+			else if (sound->nChannels==2)
+			{ alBufferData(soundBuffer,AL_FORMAT_STEREO16,sound->data,sound->nData,sound->samplesPerSecond); }
+			else
+			{ std::cout<<"Error: WAV needs to have 1 or 2 channels.\n"; }
+		}
+		else
+		{ std::cout<<"Error: Sound needs to be 8 or 16 bit.\n"; }
+
+		
+		ALuint source;
+		alGenSources(1,&source);
+		/* Load buffer into source. */
+		alSourcei(source, AL_BUFFER, soundBuffer);
+		/* Set volume. */
+		const float volume = (float)globalVolume/100;
+		alSourcef(source, AL_GAIN, volume);
+
+      vLoadedSource.push(source);
+      return vLoadedSource.size()-1;
+		// vSound.push(sound);
+		// vSource.push(source);
+#else
+   return 0;
+#endif
+   }
 	
+   void playLoaded(int id)
+   {
+#ifdef WILDCAT_AUDIO
+      alSourceStop(vLoadedSource(id));
+		/* Play source in new thread. */
+		alSourcePlay(vLoadedSource(id));
+
+#endif
+   }
+   
 	/* ALC needs to be shut down properly or it seems to hang the system if audio is playing.
 		In some cases the audio player needs to be closed manually */
 	~AudioPlayer_OpenAL()
@@ -157,6 +337,7 @@ class AudioPlayer_OpenAL: public AudioPlayer
 		
 		vSound.push(sound);
 		vSource.push(source);
+		vBuffer.push(soundBuffer);
 		
 #endif
 	}
@@ -245,7 +426,23 @@ class AudioPlayer_OpenAL: public AudioPlayer
 	
 	void garbageCollect()
 	{
-		
+#ifdef WILDCAT_AUDIO
+// This should be called periodically to free up memory from sounds that have finished playing
+         for (int i=0;i<vSource.size();++i)
+         {
+            ALenum state;
+            alGetSourcei(vSource(i), AL_SOURCE_STATE, &state);
+            if (state != AL_PLAYING)
+            {
+               alDeleteSources(1,&vSource(i));
+               alDeleteBuffers(1,&vBuffer(i));
+               vSource(i)=0;
+               vBuffer(i)=0;
+            }
+         }
+         vSource.removeNulls();
+         vBuffer.removeNulls();
+#endif
 	}
 	
 };
