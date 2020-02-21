43d96c05e553319eb3fc9ba864431d2ab44ebad8
Ryan Babij <rbabij@gmail.com>
2020-02-21 21:59:37 +1030

    Update Timer.hpp
    * Add non-MingW Windows time code

--- System/Time/Timer.hpp
+++ System/Time/Timer.hpp
@@ -1,79 +1,98 @@
 #pragma once
-#ifndef TIME_TIMER_HPP
-#define TIME_TIMER_HPP
+#ifndef WILDCAT_SYSTEM_TIME_TIMER_HPP
+#define WILDCAT_SYSTEM_TIME_TIMER_HPP
 
-/* #include <Time/Timer.hpp>
-Linux Implementation (Seems to also work with mingw or whatever)
+/* Wildcat: Timer
+   #include <Time/Timer.hpp>
+   
+   Linux Implementation (Seems to also work with mingw or whatever)
 
-Note that tv_usec resets after each second.
+   Note that tv_usec resets after each second.
 
+   Profiling example:
 
-Profiling example:
-
-		Timer t;
-		t.init();
-		t.start();
-			// CODE TO TIME
-		t.update();
-		std::cout<<"Execution time: "<<t.fullSeconds<<".\n";
+   Timer t;
+   t.init();
+   t.start();
+   // CODE TO TIME
+   t.update();
+   std::cout<<"Execution time: "<<t.fullSeconds<<".\n";
+   
+   GetTickCount() returns milliseconds since startup
 
 */
 
-
-#include <sys/time.h>
+#if defined WILDCAT_LINUX || defined __MINGW32__
+#include <sys/time.h> // I believe this is normally Linux only, but also works with MingW.
+#else
+#endif
 
 class Timer
 {
-    public:
+   public:
+   
+#if defined WILDCAT_LINUX || defined __MINGW32__
+   timeval startTime;
+   timeval stopTime;
+#else
+   unsigned long int startTick; // milliseconds since startup
+#endif
 
-	timeval startTime;
-	timeval stopTime;
+   long unsigned int uSeconds; /* This goes only as far as 1 second before resetting. See documentation. */
+   unsigned int seconds;
+   double fullSeconds;
 
-    long unsigned int uSeconds; /* This goes only as far as 1 second before resetting. See documentation. */
-	unsigned int seconds;
-	double fullSeconds;
-	
-	long int totalUSeconds;
+   long int totalUSeconds;
 
-  Timer()
-  {
-    init();
-  }
-	void init()
-	{
-		uSeconds=0;
-		seconds=0;
-		fullSeconds=0;
-		totalUSeconds=0;
-	}
-  void start()
-  {
-    gettimeofday(&startTime,0);
-  }
-	void update()
-	{
-		gettimeofday(&stopTime,0);
+   Timer()
+   {
+      init();
+   }
+   void init()
+   {
+      uSeconds=0;
+      seconds=0;
+      fullSeconds=0;
+      totalUSeconds=0;
+#if defined WILDCAT_LINUX || defined __MINGW32__
+#else
+      startTick=0;
+#endif
+   }
+   void start()
+   {
+#if defined WILDCAT_LINUX || defined __MINGW32__
+      gettimeofday(&startTime,0);
+#else
+   startTick = GetTickCount();
+#endif
+   }
+   void update()
+   {
+#if defined WILDCAT_LINUX || defined __MINGW32__
+      gettimeofday(&stopTime,0);
+      seconds=stopTime.tv_sec-startTime.tv_sec;
 
-		seconds=stopTime.tv_sec-startTime.tv_sec;
-		
-		totalUSeconds = stopTime.tv_usec-startTime.tv_usec + (seconds*1000000);
-		
-		if(stopTime.tv_usec<startTime.tv_usec)
-		{
-			stopTime.tv_usec+=1000000;
-			seconds--;
-		}
-		uSeconds=stopTime.tv_usec-startTime.tv_usec;
-		
-		fullSeconds=seconds+((double)uSeconds/1000000);
-	}
-	
-	// void addUSeconds(unsigned long int uSeconds)
-	// {
-		// uSeconds+=
-	
-	// }
-	
+      totalUSeconds = stopTime.tv_usec-startTime.tv_usec + (seconds*1000000);
+      if(stopTime.tv_usec<startTime.tv_usec)
+      {
+         stopTime.tv_usec+=1000000;
+         seconds--;
+      }
+      uSeconds=stopTime.tv_usec-startTime.tv_usec;
+      
+      fullSeconds=seconds+((double)uSeconds/1000000);
+#else
+      long unsigned int endTick = GetTickCount();
+      long unsigned int totalTicks = endTick-startTick;
+      
+      uSeconds = (totalTicks%1000)*1000;
+      totalUSeconds = totalTicks*1000;
+      seconds=totalTicks/1000;
+      fullSeconds=seconds+((double)uSeconds/1000000);
+#endif
+   }
+
 };
 
 #endif
