4b548bb10616977605a4337d4046c23778abe92b
Ryan Babij <rbabij@gmail.com>
2020-06-24 16:28:24 +1000

    Update Sleep.hpp
    Remove OS-specific function defs, to prevent accidental use of them
    Cleanup

--- System/Sleep/Sleep.hpp
+++ System/Sleep/Sleep.hpp
@@ -1,10 +1,11 @@
 #pragma once
-#ifndef SLEEP_SLEEP_HPP
-#define SLEEP_SLEEP_HPP
+#ifndef SYSTEM_SLEEP_SLEEP_HPP
+#define SYSTEM_SLEEP_SLEEP_HPP
 
-/*
-#include <Sleep/Sleep.hpp>
-Cross-platform sleep function.
+/* Wildcat: Sleep
+#include <System/Sleep/Sleep.hpp> */
+
+/** Cross-platform sleep function.
 
 For Windows it calls Sleep(milliseconds).
 For Unix it calls usleep(microseconds).
@@ -14,8 +15,9 @@ A microsecond it 1/1,000,000th of a second.
 
 Milliseconds should provide a poll-rate of around 1000 polls per second, which should be good enough for both Windows and Unix, and therefore both sleep functions are converted to milliseconds.
 
-*/
+In Windows, Sleep(0) will make the thread relinquish its remaining time to any other threads which need to run.
 
+*/
 
 #ifdef WILDCAT_WINDOWS
 #include <windows.h>
@@ -24,31 +26,25 @@ Milliseconds should provide a poll-rate of around 1000 polls per second, which s
 #include <unistd.h>
 #endif
 
+// prevent accidentally using the OS-specific sleep functions
+#define Sleep_Windows Sleep
+#define Sleep_Linux usleep
+#undef Sleep
+#undef usleep
+
 /* sleep(1000) = one second. */
-void sleep(const int milliseconds)
+void sleep(const unsigned int milliseconds)
 {
 	#ifdef WILDCAT_WINDOWS
-		Sleep(milliseconds);
+		Sleep_Windows(milliseconds);
 	#elif defined WILDCAT_LINUX
-		usleep( milliseconds * 1000 );
+		Sleep_Linux( milliseconds * 1000 );
 	#else
-	#error SLEEP_SLEEP_HPP needs WILDCAT_WINDOWS or WILDCAT_LINUX define.
+		#error SLEEP_SLEEP_HPP needs WILDCAT_WINDOWS or WILDCAT_LINUX define.
 	#endif
 }
 
-void sleep ( const double milliseconds )
-{
-	#ifdef WILDCAT_WINDOWS
-		Sleep(milliseconds);
-	#elif defined WILDCAT_LINUX
-		usleep( milliseconds * 1000 );
-	#else
-	#error SLEEP_SLEEP_HPP needs WILDCAT_WINDOWS or WILDCAT_LINUX define.
-	#endif
-}
-
-void sleepSeconds(const int seconds)
+void sleepSeconds(const unsigned int seconds)
 { sleep(seconds*1000); }
-
 
 #endif /* #ifndef SLEEP_SLEEP_HPP */
