d28dca4c3d48f04a7dfdbc3ce4dcc239eec64184
Ryan Babij <rbabij@protonmail.com>
2021-04-20 13:20:19 +1000

    OpenGL: Pull glew typedefs into own header
    Some of my headers need OpenGL typedefs so I decided to put them in their own header so I don't need to include 20,000 line files multiple times

---
+++ Graphics/OpenGL/glew-typedefs.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#ifndef WILDCAT_OPENGL_GLEW_TYPEDEFS_HPP
+#define WILDCAT_OPENGL_GLEW_TYPEDEFS_HPP
+
+// #include <Graphics/OpenGL/glew-typedefs.hpp>
+
+// I moved the typedefs out of glew.h because some of my classes need only these and not the other 22,000 lines
+
+typedef unsigned int GLenum;
+typedef unsigned int GLbitfield;
+typedef unsigned int GLuint;
+typedef int GLint;
+typedef int GLsizei;
+typedef unsigned char GLboolean;
+typedef signed char GLbyte;
+typedef short GLshort;
+typedef unsigned char GLubyte;
+typedef unsigned short GLushort;
+typedef unsigned long GLulong;
+typedef float GLfloat;
+typedef float GLclampf;
+typedef double GLdouble;
+typedef double GLclampd;
+typedef void GLvoid;
+#if defined(_MSC_VER) && _MSC_VER < 1400
+typedef __int64 GLint64EXT;
+typedef unsigned __int64 GLuint64EXT;
+#elif defined(_MSC_VER) || defined(__BORLANDC__)
+typedef signed long long GLint64EXT;
+typedef unsigned long long GLuint64EXT;
+#else
+#  if defined(__MINGW32__) || defined(__CYGWIN__)
+#include <inttypes.h>
+#  endif
+typedef int64_t GLint64EXT;
+typedef uint64_t GLuint64EXT;
+#endif
+typedef GLint64EXT  GLint64;
+typedef GLuint64EXT GLuint64;
+typedef struct __GLsync *GLsync;
+
+typedef char GLchar;
+
+#endif
--- Graphics/OpenGL/glew.h
+++ Graphics/OpenGL/glew.h
@@ -1,3 +1,7 @@
+#pragma once
+#ifndef WLIDCAT_OPENGL_GLEW_HPP
+#define WLIDCAT_OPENGL_GLEW_HPP
+
 /*
 ** The OpenGL Extension Wrangler Library
 ** Copyright (C) 2008-2017, Nigel Stewart <nigels[]users sourceforge net>
@@ -275,40 +279,7 @@ extern "C" {
 #ifndef GL_VERSION_1_1
 #define GL_VERSION_1_1 1
 
-typedef unsigned int GLenum;
-typedef unsigned int GLbitfield;
-typedef unsigned int GLuint;
-typedef int GLint;
-typedef int GLsizei;
-typedef unsigned char GLboolean;
-typedef signed char GLbyte;
-typedef short GLshort;
-typedef unsigned char GLubyte;
-typedef unsigned short GLushort;
-typedef unsigned long GLulong;
-typedef float GLfloat;
-typedef float GLclampf;
-typedef double GLdouble;
-typedef double GLclampd;
-typedef void GLvoid;
-#if defined(_MSC_VER) && _MSC_VER < 1400
-typedef __int64 GLint64EXT;
-typedef unsigned __int64 GLuint64EXT;
-#elif defined(_MSC_VER) || defined(__BORLANDC__)
-typedef signed long long GLint64EXT;
-typedef unsigned long long GLuint64EXT;
-#else
-#  if defined(__MINGW32__) || defined(__CYGWIN__)
-#include <inttypes.h>
-#  endif
-typedef int64_t GLint64EXT;
-typedef uint64_t GLuint64EXT;
-#endif
-typedef GLint64EXT  GLint64;
-typedef GLuint64EXT GLuint64;
-typedef struct __GLsync *GLsync;
-
-typedef char GLchar;
+#include <Graphics/OpenGL/glew-typedefs.hpp>
 
 #define GL_ZERO 0
 #define GL_FALSE 0
@@ -23684,3 +23655,5 @@ GLEWAPI const GLubyte * GLEWAPIENTRY glewGetString (GLenum name);
 /* #undef GLEWAPI */
 
 #endif /* __glew_h__ */
+
+#endif
