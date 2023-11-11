37beb8ee294dcd45b5996873f55892f1555ef110
Ryan <20412306+RyanBabij@users.noreply.github.com>
2023-11-11 12:11:48 +1030

    Expand LanguageTools
    Add vowel and consonant checks
    Add random vowel and consonant get

--- Game/Language/LanguageTools.cpp
+++ Game/Language/LanguageTools.cpp
@@ -9,17 +9,136 @@
 
 namespace LanguageTools
 {
-    // Initialize the static constant array
-    const char VOWELS[] = {'a', 'e', 'i', 'o', 'u', 's', 'y'};
-    const size_t NUM_VOWELS = sizeof(VOWELS) / sizeof(VOWELS[0]);
+	// Initialize the static constant array
+	const char VOWELS[] = {'a', 'e', 'i', 'o', 'u', 's', 'y'};
+	const size_t NUM_VOWELS = sizeof(VOWELS) / sizeof(VOWELS[0]);
 
-    // Initialize the static constant array of consonants
-    const char CONSONANTS[] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'y'};
-    const size_t NUM_CONSONANTS = sizeof(CONSONANTS) / sizeof(CONSONANTS[0]);
+	// Initialize the static constant array of consonants
+	const char CONSONANTS[] = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'r', 's', 't', 'v', 'w', 'y'};
+	const size_t NUM_CONSONANTS = sizeof(CONSONANTS) / sizeof(CONSONANTS[0]);
 
-    // Initialize the static constant array of double consonants
-    const std::string DOUBLE_CONSONANTS[] = {"ch", "sh", "ph", "rh", "wh", "ck", "st", "ll", "qu", "ss", "nz", "ld", "hn", "gg", "gh", "fr", "rx", "rm"};
-    const size_t NUM_DOUBLE_CONSONANTS = sizeof(DOUBLE_CONSONANTS) / sizeof(DOUBLE_CONSONANTS[0]);
+	// Initialize the static constant array of double consonants
+	const std::string DOUBLE_CONSONANTS[] = {"ch", "sh", "ph", "rh", "wh", "ck", "st", "ll", "qu", "ss", "nz", "ld", "hn", "gg", "gh", "fr", "rx", "rm"};
+	const size_t NUM_DOUBLE_CONSONANTS = sizeof(DOUBLE_CONSONANTS) / sizeof(DOUBLE_CONSONANTS[0]);
+
+
+	bool isVowel(char ch)
+	{
+		ch = tolower(ch); // Convert to lowercase to handle uppercase input
+
+		for (char vowel : VOWELS)
+		{
+			if (ch == vowel)
+			{
+				return true; // Return true if the character matches a vowel
+			}
+		}
+		return false; // Return false if no match is found
+	}
+	
+	bool isVowel(const std::string& str)
+	{
+		if (str.length() != 1)
+		{
+			return false; // Return false if the string is not a single character
+		}
+		return isVowel(str[0]);
+	}
+	
+	char isVowelGet(const std::string& str)
+	{
+		if (str.length() != 1)
+		{
+			return 0; // Return false if the string is not a single character
+		}
+
+		char ch = tolower(str[0]); // Convert to lowercase to handle uppercase input
+
+		for (char vowel : VOWELS)
+		{
+			if (ch == vowel)
+			{
+				return ch; // Return true if the character matches a vowel
+			}
+		}
+
+		return 0; // Return false if no match is found
+	}
+	
+	char randomVowel(RandomLehmer& rng)
+	{
+		return LanguageTools::VOWELS[rng.rand8(LanguageTools::NUM_VOWELS)];
+	}
+	char randomVowelExcept(RandomLehmer& rng, char excludeVowel)
+	{
+		char vowel;
+		do
+		{
+			vowel = randomVowel(rng);
+		}
+		while (vowel == excludeVowel);
+
+		return vowel;
+	}
+	
+	
+	bool isConsonant(char ch)
+	{
+		ch = tolower(ch); // Convert to lowercase to handle uppercase input
+
+		for (char consonant : CONSONANTS)
+		{
+			if (ch == consonant)
+			{
+				return true; // Return true if the character matches a consonant
+			}
+		}
+		return false; // Return false if no match is found
+	}
+	bool isConsonant(const std::string& str)
+	{
+		if (str.length() != 1)
+		{
+			return false; // Return false if the string is not a single character
+		}
+		return isConsonant(str[0]);
+	}
+	char isConsonantGet(const std::string& str)
+	{
+		if (str.length() != 1)
+		{
+			return 0; // Return false if the string is not a single character
+		}
+
+		char ch = tolower(str[0]); // Convert to lowercase to handle uppercase input
+
+		for (char consonant : CONSONANTS)
+		{
+			if (ch == consonant)
+			{
+				return ch; // Return true if the character matches a consonant
+			}
+		}
+
+		return 0; // Return false if no match is found
+	}
+	
+	char randomConsonant(RandomLehmer& rng)
+	{
+		return LanguageTools::CONSONANTS[rng.rand8(LanguageTools::NUM_CONSONANTS)];
+	}
+	char randomConsonantExcept(RandomLehmer& rng, char excludeConsonant)
+	{
+		char consonant;
+		do
+		{
+			consonant = randomConsonant(rng);
+		}
+		while (consonant == excludeConsonant);
+
+		return consonant;
+	}
+
 }
 
 #endif // WILDCAT_LANGUAGE_LANGUAGE_TOOLS_CPP
--- Game/Language/LanguageTools.hpp
+++ Game/Language/LanguageTools.hpp
@@ -12,15 +12,32 @@
 #include <string>
 #include <cstddef> // For size_t
 
-namespace LanguageTools {
-    extern const char VOWELS[];
-    extern const size_t NUM_VOWELS;
+namespace LanguageTools
+{
+	extern const char VOWELS[];
+	extern const size_t NUM_VOWELS;
 
-    extern const char CONSONANTS[];
-    extern const size_t NUM_CONSONANTS;
+	extern const char CONSONANTS[];
+	extern const size_t NUM_CONSONANTS;
 
-    extern const std::string DOUBLE_CONSONANTS[];
-    extern const size_t NUM_DOUBLE_CONSONANTS;
+	extern const std::string DOUBLE_CONSONANTS[];
+	extern const size_t NUM_DOUBLE_CONSONANTS;
+	
+	bool isVowel(char ch);
+	bool isVowel(const std::string& str);
+		// Return the vowel if the passed string is a vowel, otherwise return 0.
+	char isVowelGet(const std::string& str);
+	
+	char randomVowel(RandomLehmer& rng);
+	char randomVowelExcept(RandomLehmer& rng, char vowel);
+	
+	bool isConsonant(char ch);
+	bool isConsonant(const std::string& str);
+		// Return the vowel if the passed string is a vowel, otherwise return 0.
+	char isConsonantGet(const std::string& str);
+	
+	char randomConsonant(RandomLehmer& rng);
+	char randomConsonantExcept(RandomLehmer& rng, char vowel);
 }
 
 #endif // WILDCAT_LANGUAGE_LANGUAGE_TOOLS_HPP
